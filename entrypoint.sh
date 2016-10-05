f7f507bccfd5529d610a13d550c8f9ecc0528932
gbolo <george.bolo@gmail.com>
2016-10-04 22:45:28 -0400

    script name changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # on container start, lets scan user configs
-/etc/periodic/daily/letsencrypt-cron.sh -c
+/etc/periodic/daily/letsencrypt-cron -c
 
-# now lets just start crond in foreground
+# now lets just start crond in foreground with max verbose
 /usr/sbin/crond -f -l 0
